2bb9e1ce836cceda7bd64e92bca65c0162d5fd3f
Leonardo Braga <54039202+leopesb@users.noreply.github.com>
2024-02-29 20:43:06 -0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+      
+    Scanner x1 = new Scanner(System.in);
+    float n1;
+    
+    System.out.println("Digite a primeira nota: ");
+    n1 = x1.nextFloat();
+    
+    
+    Scanner x2 = new Scanner(System.in);
+    float n2;
+    
+    System.out.println("Digite a segunda nota: ");
+    n2 = x2.nextFloat();
+    
+    float media = (n1 + n2) / 2;
+    
+    System.out.println("\nMédia do usuario: " + media);  
+    
+    if(media >= 0 && media < 4){
+        System.out.println("Reprovado!");
+    }
+    
+    else if(media < 6){
+        System.out.println("Recuperação!");
+    }
+    
+    else if(media < 9){
+        System.out.println("Aprovado!");
+    }
+    
+    else if (media >= 9){
+        System.out.println("Aprovado com excelência!");
+    }
+  }
+}
