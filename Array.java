75d81dc3ecc29ca2bdc0d6e796d69fd2f79d46c2
Leonardo Braga <54039202+leopesb@users.noreply.github.com>
2024-03-11 22:22:26 -0300

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,14 @@
+public class Array {
+    public static void main(String[] args) {
+        
+        String[] name = {"", "Leonardo", "João", "Sinclair"};
+        int[] num = {10, 20, 30, 40, 50};
+
+        System.out.println(num[0]);
+        System.out.println(name[2]);
+
+        /*for(int i = 0; i < lista_nomes.length; i++){
+            System.out.println(lista_nomes[i]);
+        }*/
+    }
+}
