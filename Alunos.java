8fbb594d7f878f975ea18987e5419872c8537db0
Leonardo Braga <54039202+leonardolopesb@users.noreply.github.com>
2024-04-28 18:27:29 -0300

    Create Alunos.java

---
+++ Alunos.java
@@ -0,0 +1,8 @@
+class Alunos extends Escola{
+    String[] nome = new String[3];
+    int[] matricula = new int[3];
+    int[] idade = new int[3];
+    float[] nota = new float[3];
+    
+    float media;
+}
