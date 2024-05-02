8a14917ef964c4a1d18e051f9a1bfa009af6a461
Leonardo Braga <54039202+leonardolopesb@users.noreply.github.com>
2024-05-02 20:06:56 -0300

    código do amigo chato

--- Alunos.java
+++ Alunos.java
@@ -1,8 +1,23 @@
-class Alunos extends Escola{
-    String[] nome = new String[3];
-    int[] matricula = new int[3];
-    int[] idade = new int[3];
-    float[] nota = new float[3];
+class Alunos {
+    public String nome;
+    public int matricula;
+    public int idade;
+    public float[] notas = new float[3];
     
-    float media;
+    public void addNota(float nota) {
+        for (int i = 0; i < notas.length; i++) {
+            if (notas[i] == 0) {
+                notas[i] = nota;
+                System.out.println("Nota adicionada com sucesso!");
+                return;
+            }
+        }
+        System.out.println("Limite de notas atingido (máximo 3 notas).");
+    }
+    
+    public void alterarNota(int index, float novaNota) {
+        if (index >= 0 && index < notas.length) {
+            notas[index] = novaNota;
+        }
+    }
 }
