bb2636b2dd2b05a00679feeff4150ac6005abc85
Leonardo Braga <54039202+leonardolopesb@users.noreply.github.com>
2024-05-02 21:24:14 -0300

    Update Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -10,23 +10,30 @@ public class Aluno extends Escola {
     
 	public void add_aluno() {
 	    Aluno al = new Aluno();
-	    
 		System.out.println("Digite o nome do aluno: ");
 		al.nome = sn.next();
-		
 		System.out.println("Digite a data de nascimento do aluno: DD/MM/AAAA ");
 		al.data_nascimento = sn.next();
-		
 		System.out.println("Digite o nome do pai do aluno: ");
 		al.nome_pai = sn.next();
-		
-		System.out.println("Digite o nome da mãe do aluno: ");
+		System.out.println("Digite o nomeda mãe do aluno: ");
 		al.nome_mae = sn.next();
-		
 		System.out.println("Digite o numero para contato do aluno: ");
 		al.contato = sn.next();
-		
 		System.out.println("Digite a matricula do aluno: ");
 		al.matricula = sn.nextInt();
 	}
+		public void mostrar_aluno() {
+            System.out.println("****************************************************");
+            
+            System.out.println("FICHA DO ALUNO");
+            System.out.println("Nome: " + this.nome);
+	        System.out.println("Data de Nascimento: " + this.data_nascimento);
+            System.out.println("Nome do pai: " + this.nome_pai);
+            System.out.println("Nome da mãe: " + this.nome_mae);
+            System.out.println("Numero para contotato: " + this.contato);
+            System.out.println("Numero da Matrícula: " + this.matricula);
+            
+            System.out.println("****************************************************");
+	   }
 }
