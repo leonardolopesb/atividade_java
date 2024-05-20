f238e34c53aa91ec269c74dbd620c38d8cf3c27c
Leonardo Braga <54039202+leonardolopesb@users.noreply.github.com>
2024-05-19 21:36:49 -0300

    Update and rename Alunos.java to Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-public class Alunos {
+public class Main {
     public static void main(String[] args) {
         Scanner sn = new Scanner(System.in);
-
+        
         int entrada = 0;
         int num_alunos = 0;
-
+        
         Aluno[] alunos = new Aluno[1000];
-
+        
         while (entrada != 6) {
             System.out.println("1 - Para adicionar alunos (e seus dados pessoais).");
             System.out.println("2 - Para acrescentar notas aos alunos.");
@@ -16,33 +16,33 @@ public class Alunos {
             System.out.println("4 - Para consultar os alunos (e seus dados pessoais).");
             System.out.println("5 - Para consultar os alunos (e suas notas/médias).");
             System.out.println("6 - Sair.");
-
+            
             System.out.print("\nDigite uma das opções: ");
             entrada = sn.nextInt();
-
+            
             switch (entrada) {
                 case 1:
                     System.out.println("Adicionando Alunos...");
-
-                    Aluno al = new AlunoRegular();
+                    
+                    Aluno al = new Aluno();
                     al.add_aluno();
-
+                    
                     alunos[num_alunos] = al;
                     num_alunos++;
-
+                    
                     System.out.println("");
                     break;
-
+                    
                 case 2:
                     System.out.println("\nAcrescentando notas aos alunos...");
-
+                    
                     if (num_alunos > 0) {
                         System.out.print("Digite a matrícula do aluno: ");
                         int checarMatricula = sn.nextInt();
-
+                        
                         boolean alunoEncontrado = false;
                         for (int i = 0; i < num_alunos; i++) {
-                            if (checarMatricula == alunos[i].matricula) {
+                            if (checarMatricula == alunos[i].getMatricula()) {
                                 System.out.print("Digite a nota do aluno: ");
                                 float nota = sn.nextFloat();
                                 alunos[i].addNota(nota);
@@ -50,31 +50,31 @@ public class Alunos {
                                 break;
                             }
                         }
-
+                        
                         if (!alunoEncontrado) {
                             System.out.println("Aluno não encontrado.\n");
                         }
                     } else {
                         System.out.println("Não há alunos para adicionar notas.\n");
                     }
-
+                    
                     break;
-
+                
                 case 3:
                     System.out.println("\nAlterando notas dos alunos...");
                     if (num_alunos > 0) {
                         System.out.print("Digite a matrícula do aluno: ");
                         int checarMatricula = sn.nextInt();
-
+                        
                         boolean alunoEncontrado = false;
                         for (int i = 0; i < num_alunos; i++) {
-                            if (checarMatricula == alunos[i].matricula) {
+                            if (checarMatricula == alunos[i].getMatricula()) {
                                 System.out.println("Notas atuais do aluno:");
-
-                                for (int j = 0; j < alunos[i].notas.length; j++) {
-                                    System.out.println("Nota " + (j + 1) + ": " + alunos[i].notas[j]);
+                                
+                                for (int j = 0; j < alunos[i].getNotas().length; j++) {
+                                    System.out.println("Nota " + (j + 1) + ": " + alunos[i].getNotas()[j]);
                                 }
-
+                                
                                 System.out.print("Digite o número da nota a ser alterada (1, 2, ou 3): ");
                                 int notaIndex = sn.nextInt();
 
@@ -97,40 +97,42 @@ public class Alunos {
                         System.out.println("Não há alunos para alterar notas.\n");
                     }
                     break;
-
+                
                 case 4:
                     System.out.println("\nConsultando dados pessoais dos alunos...");
 
                     if (num_alunos > 0) {
                         for (int i = 0; i < num_alunos; i++) {
-                            System.out.println("\t\t\t\t\t\t#### DADOS PESSOAIS DO ALUNO DE MATRÍCULA: " + alunos[i].matricula);
-                            alunos[i].mostrar_aluno();
+                            System.out.println("\t\t\t\t\t\t#### DADOS PESSOAIS DO ALUNO DE MATRÍCULA: " + alunos[i].getMatricula());
+                            alunos[i].mostrar_dados();
                             System.out.println();
                         }
-                    } else {
+                    } 
+                    else {
                         System.out.println("Não há alunos para consultar.\n");
                     }
                     break;
 
+                    
                 case 5:
                     System.out.println("\nConsultando notas/médias dos alunos...");
-
+                    
                     if (num_alunos > 0) {
                         for (int i = 0; i < num_alunos; i++) {
-                            System.out.println("\t\t\t\t\t\t#### NOTAS/MÉDIAS DO ALUNO " + (i + 1) + " #####");
-                            System.out.println("\t\t\t\t\t\t#### Nome: " + alunos[i].nome);
-                            System.out.println("\t\t\t\t\t\t#### Matrícula: " + alunos[i].matricula);
-
+                            System.out.println("\t\t\t\t\t\t#### NOTAS/MÉDIAS DO ALUNO " + (i+1) + " #####");
+                            System.out.println("\t\t\t\t\t\t#### Nome: " + alunos[i].getNome());
+                            System.out.println("\t\t\t\t\t\t#### Matrícula: " + alunos[i].getMatricula());
+                            
                             float somaNotas = 0;
                             int numNotas = 0;
-                            for (float nota : alunos[i].notas) {
+                            for (float nota : alunos[i].getNotas()) {
                                 if (nota != 0) {
                                     System.out.println("\t\t\t\t\t\tNota " + (numNotas + 1) + ": " + nota);
                                     somaNotas += nota;
                                     numNotas++;
                                 }
                             }
-
+                            
                             if (numNotas > 0) {
                                 float media = somaNotas / numNotas;
                                 System.out.println("\t\t\t\t\t\tMédia: " + media);
@@ -143,11 +145,11 @@ public class Alunos {
                         System.out.println("Não há alunos para consultar notas/médias.\n");
                     }
                     break;
-
+                
                 case 6:
                     System.out.println("\nBye Bye!");
                     break;
-
+                    
                 default:
                     System.out.println("Opção Inválida! Por favor, selecione uma das opções abaixo:\n");
             }
