2c407acae766c04d704eca31762baf1187a54160
Leonardo Braga <54039202+leonardolopesb@users.noreply.github.com>
2024-05-19 21:18:44 -0300

    Acréscimo da classe abstrata 'Aluno'

--- Aluno.java
+++ Aluno.java
@@ -1,64 +1,62 @@
 import java.util.Scanner;
 
-class Aluno extends Escola{
+abstract class Aluno {
     public String nome;
-    public int matricula;
     public int idade;
-    public float[] notas = new float[3];
-    public String data_nascimento;
-    public String nome_pai;
-    public String nome_mae;
-    public String contato;
+    protected int matricula;
+    protected float[] notas = new float[3];
     Scanner sn = new Scanner(System.in);
 
     public void add_aluno() {
         System.out.print("Digite o nome do aluno: ");
         this.nome = sn.next();
         
-        System.out.print("Digite a data de nascimento do aluno (DD/MM/AAAA): ");
-        this.data_nascimento = sn.next();
-        
-        System.out.print("Digite o nome do pai do aluno: ");
-        this.nome_pai = sn.next();
-        
-        System.out.print("Digite o nome da mãe do aluno: ");
-        this.nome_mae = sn.next();
-        
-        System.out.print("Digite o número para contato do aluno: ");
-        this.contato = sn.next();
+        System.out.print("Digite a idade do aluno: ");
+        this.idade = sn.nextInt();
         
         System.out.print("Digite a matrícula do aluno: ");
         this.matricula = sn.nextInt();
     }
-    
+
+
     public void mostrar_aluno() {
         System.out.println();
         System.out.println("\t\t\t\t\t\t#### FICHA DO ALUNO");
         System.out.println("\t\t\t\t\t\t#### Nome: " + this.nome);
-        System.out.println("\t\t\t\t\t\t#### Data de Nascimento: " + this.data_nascimento);
-        System.out.println("\t\t\t\t\t\t#### Nome do pai: " + this.nome_pai);
-        System.out.println("\t\t\t\t\t\t#### Nome da mãe: " + this.nome_mae);
-        System.out.println("\t\t\t\t\t\t#### Número para contato: " + this.contato);
+        System.out.println("\t\t\t\t\t\t#### Idade: " + this.idade);
         System.out.println("\t\t\t\t\t\t#### Número da Matrícula: " + this.matricula);
-        System.out.println("****************************************************");
     }
 
     public void addNota(float nota) {
         for (int i = 0; i < notas.length; i++) {
             if (notas[i] == 0) {
                 notas[i] = nota;
-                System.out.println("Nota adicionada com sucesso!");
+                System.out.println("Nota adicionada com sucesso!\n");
                 return;
             }
         }
-        System.out.println("Limite de notas atingido (máximo 3 notas).");
+        System.out.println("Limite de notas atingido (máximo 3 notas).\n");
     }
-    
+
     public void alterarNota(int index, float novaNota) {
         if (index >= 0 && index < notas.length) {
             notas[index] = novaNota;
         } else {
             System.out.println("Índice de nota inválido.");
         }
+    }
+}
+
+class AlunoRegular extends Aluno {
+    @Override
+    public void add_aluno() {
+        System.out.print("Digite o nome do aluno: ");
+        this.nome = sn.next();
+
+        System.out.print("Digite a idade do aluno: ");
+        this.idade = sn.nextInt();
+
+        System.out.print("Digite a matrícula do aluno: ");
+        this.matricula = sn.nextInt();
     }
 }
